92a22eea5aca0c1e5aea7f16e57b211c8b68ca97
Ruba124 <r.m.d.abkam@gmail.com>
2023-11-01 02:35:44 +0200

    sksk

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for letter in range(97, 123):
+    lo = chr(l)
+    print("{}".format(lo), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for letter in range(97,101) + range(102,113) + range(114,123):
+    new = chr(letter)
+    print("{}".format(new))
